a9b24e0ec092ae08926cba0b7f395e5b63e73d3b
Quantisol20 <103952586+Quantisol20@users.noreply.github.com>
2024-08-24 13:06:54 +0100

    FixVariable

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
+variable "AWS_ACCESS_KEY_ID" {
+    description = "AWS access key"
+    type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+    description = "AWS secret access key"
+    type = string
+}
+
 provider "aws" {
     region = "us-east-1"
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_instance" "csn" {
